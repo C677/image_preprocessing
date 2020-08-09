1be17fbaaa950a135024b59debb35e6d27547237
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-08-09 16:44:26 +0900

    edit bug

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,24 +2,31 @@ import csv
 import json
 from collections import OrderedDict
 
-csv_filename = 'tcia_labels.csv'
-# csv_filename = 'covid(1번)_annotation.csv'
-# csv_filename = 'covid(Larxel)_annotations.csv'
-# csv_filename = 'covid(MedSeg)_annotations.csv'
-# csv_filename = 'nodule_annotations.csv'
+csv_filename = 'train.csv'
+# csv_filename = 'val.csv'
+# csv_filename = 'test.csv'
+
 
 f = open(f'{csv_filename}', 'r', encoding='utf-8')
 
 rdr = csv.reader(f)
 
-image_id = -1
+image_id = 0
 filename = ""
+upper_group = dict()
 group = dict()
-
+line_count = 0
 for line in rdr:
+
+    if line_count == 0:
+        line_count = line_count +1
+        continue
     if line[0] == filename:
         annotation = dict()
-        annotation["bbox"] = [line[1], line[3], line[2], line[4]]
+        print(line[1])
+        print(type(line[1]))
+
+        annotation["bbox"] = [int(float(line[1])), int(float(line[3])), int(float(line[2])), int(float(line[4]))]
         annotation["bbox_mode"] = 0  # X0,Y0,X1,Y1
         if line[5] == "cancer":
             annotation["category_id"] = 0
@@ -30,10 +37,8 @@ for line in rdr:
         group["annotations"].append(annotation)
         continue
     else:
-        print(image_id-1)
-        print(filename)
-        with open(f'json\\{csv_filename}\\{filename}.json', 'w', encoding='utf-8') as make_file:
-            json.dump(group, make_file, indent="\t")
+        upper_group[f'{filename}'] = group
+        group = dict()
 
     filename = line[0]
     group["image_id"] = image_id
@@ -41,11 +46,11 @@ for line in rdr:
     group["height"] = 512
     group["width"] = 512
     annotation = dict()
-    annotation["bbox"] = [line[1], line[3], line[2], line[4]]
+    annotation["bbox"] = [int(float(line[1])), int(float(line[3])), int(float(line[2])), int(float(line[4]))]
     annotation["bbox_mode"] = 0  # X0,Y0,X1,Y1
     if line[5] == "cancer":
         annotation["category_id"] = 0
-    elif line[5] == "covid":
+    elif line[5] == "covid-19":
         annotation["category_id"] = 1
     elif line[5] == "nodule":
         annotation["category_id"] = 2
@@ -53,8 +58,10 @@ for line in rdr:
     group["annotations"].append(annotation)
     image_id = image_id + 1
 
-with open(f'json\\{csv_filename}\\{filename}.json', 'w', encoding='utf-8') as make_file:
-    json.dump(group, make_file, indent="\t")
+upper_group[f'{filename}'] = group
+
+with open(f'json\\{csv_filename}.json', 'w', encoding='utf-8') as make_file:
+    json.dump(upper_group, make_file, indent="\t")
 print(image_id-1)
 print(filename)
 
